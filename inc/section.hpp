d4dd7eb4e22987dc14ec8ee85e996990bc10d971
Predrag Perisic <predrag3.14159@gmail.com>
2021-09-12 07:12:56 +0200

    Define section header table entry

--- inc/section.hpp
+++ inc/section.hpp
@@ -8,9 +8,33 @@
 #include "types.hpp"
 #include "symbol.hpp"
 
+enum SectionType: ubyte
+{
+    ST_NONE = 0,
+    ST_DATA,   // code and data
+    ST_REL,    // section containing relocation entries
+    ST_STR, // section containing symbol identifiers
+};
+
+struct SectionEntry
+{
+    SectionEntry() :
+        type(ST_NONE)
+    {}
+
+    SectionType type;
+    ushort nameOffset; // offset in .str section
+    uint dataOffset; // section data offset
+};
+
 struct Section
 {
+    Section() :
+        entry(nullptr)
+    {}
+
     std::vector<ubyte> data;
+    SectionEntry *entry;
 };
 
 typedef std::unordered_map<std::string, Section> SectionTable;
